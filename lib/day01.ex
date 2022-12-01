6fbf4bbb1bc4e00f7847423f2d20612e464fc849
Trevik <trevik.peterson@angel.com>
2022-11-30 23:36:44 -0700

    Day01 part 1
    using mix test to run the function

--- lib/day01.ex
+++ lib/day01.ex
@@ -1,5 +1,17 @@
 defmodule Day01 do
-  def todo do
-    :yo
+  def calorie_count do
+    {:ok, contents} = File.read("lib/day01input.txt")
+
+    contents
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(fn calories -> convert_and_sum(calories) end)
+    |> Enum.max()
+    |> IO.inspect()
+  end
+
+  defp convert_and_sum(calories) do
+    String.split(calories, "\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
   end
 end
--- test/advent_of_code22_test.exs
+++ test/advent_of_code22_test.exs
@@ -3,6 +3,6 @@ defmodule AdventOfCode22Test do
   doctest AdventOfCode22
 
   test "greets the world" do
-    assert AdventOfCode22.hello() == :world
+    Day01.calorie_count()
   end
 end
